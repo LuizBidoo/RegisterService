242b7743a6f653a63914180c4b99d1068b0a737d
Luiz Bido <lfsbido@inf.ufpel.edu.br>
2024-08-04 11:08:16 -0300

    feat: db connection authentication added

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express') // importando express
+const db = require('./db/connection') // importando o ponteiro pro db
 const app = express() // iniciando o app como uma instancia de express
 
 const port = 3001
@@ -7,6 +8,12 @@ app.listen(port, () => {
     console.log(`Servidor conectado a porta ${port}`)
 })
 
-app.get('/', (req, res) => {
+// conexão com o db
+db.authenticate()
+    .then(() => console.log('Db Connected'))
+    .catch(err => console.log('Erro ao conectar ao DB', err))
+
+// rotas
+app.get('/', (req, res) => { 
     res.send('Hello world')
 })

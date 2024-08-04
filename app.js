f45403a30787fe592539a65e0dd379306e2f3bea
Luiz Bido <lfsbido@inf.ufpel.edu.br>
2024-08-04 11:48:54 -0300

    feat: POST route to add registers in table created

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express') // importando express
 const db = require('./db/connection') // importando o ponteiro pro db
 const app = express() // iniciando o app como uma instancia de express
+const bodyParser = require('body-parser') // iniciando o body-parser para a ler o corpo da req
+const routesRegister = require('./routes/registers')
 
 const port = 3001
 
@@ -8,12 +10,18 @@ app.listen(port, () => {
     console.log(`Servidor conectado a porta ${port}`)
 })
 
+// utilizando o bodyparser para ler o corpo da requisição
+app.use(bodyParser.urlencoded({extended: false}))
+
 // conexão com o db
 db.authenticate()
     .then(() => console.log('Db Connected'))
     .catch(err => console.log('Erro ao conectar ao DB', err))
 
-// rotas
+// rota principal
 app.get('/', (req, res) => { 
-    res.send('Hello world')
+    res.send('Página principal')
 })
+
+// rota para todos os métodos
+app.use('/register', routesRegister)
--- routes/registers.js
+++ routes/registers.js
@@ -1,1 +1,23 @@
 const express = require('express')
+const router = express.Router()
+const Register = require('../models/Register')
+
+// rota GET teste
+router.get('/test', (req, res) => {
+    res.send('Deu certo')
+}) 
+
+// rota POST para vagas
+router.post('/add', (req, res) => {
+  let {name, email, date, tele} = req.body // dados vindos do body
+  Register.create({
+    name,
+    email,
+    date,
+    tele
+  })
+  .then(() => res.redirect('/')) // volta pra home
+  .catch(err => console.log('Erro ao criar cadastro', err))
+})
+
+module.exports = router

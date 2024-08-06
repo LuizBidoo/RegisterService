43e0dfec771b2a517a946d83a39ace9c8499f657
Luiz Bido <lfsbido@inf.ufpel.edu.br>
2024-08-06 01:06:22 -0300

    style: translating comments to en

--- app.js
+++ app.js
@@ -1,27 +1,28 @@
-const express = require('express') // importando express
-const db = require('./db/connection') // importando o ponteiro pro db
-const app = express() // iniciando o app como uma instancia de express
-const bodyParser = require('body-parser') // iniciando o body-parser para a ler o corpo da req
-const routesRegister = require('./routes/registers')
+const express = require('express') 
+const db = require('./db/connection') // importing db pointer
+const app = express()
+const bodyParser = require('body-parser')
+const routesRegister = require('./routes/registers') // importing router
 
 const port = 3001
 
+// listening to port 3001
 app.listen(port, () => {
     console.log(`Servidor conectado a porta ${port}`)
 })
 
-// utilizando o bodyparser para ler o corpo da requisição
+// body-parser to read the req body
 app.use(bodyParser.urlencoded({extended: false}))
 
-// conexão com o db
+// db connection aunthentication
 db.authenticate()
     .then(() => console.log('Db Connected'))
     .catch(err => console.log('Erro ao conectar ao DB', err))
 
-// rota principal
+// route / (main page)
 app.get('/', (req, res) => { 
     res.send('Página principal')
 })
 
-// rota para todos os métodos
+// main route for all methods
 app.use('/register', routesRegister)
